5ee6781dc2e45fffc34e846a90f305ac79010960
kishu <kishu@navercorp.com>
2018-06-23 16:20:31 +0900

    refactor: product -> products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ProductModule } from './product/product.module';
+import { ProductModule } from './products/product.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
